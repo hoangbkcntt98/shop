f494a34243be9a5668b8f4e0be5389c2139c45be
hoangbkcnnt98 <20166135@student.hust.edu.vn>
2019-11-22 19:44:39 +0700

    social login1

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -12,7 +12,7 @@
  }
  public function callback($provider)
  {
-   $getInfo = Socialite::driver($provider)->stateless()->user(); 
+   $getInfo = Socialite::driver($provider)->user(); 
    $user = $this->createUser($getInfo,$provider); 
    auth()->login($user); 
    return redirect()->to('/');
